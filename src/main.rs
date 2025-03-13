553766d7fbe281a81364e9332279c59a95f345cd
Clervens Volcy <clervens.volcy@gmail.com>
2025-03-13 00:14:15 -0400

    added ws plus peers route

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,81 @@
+use std::sync::{Arc, Mutex};
+use std::net::SocketAddr;
 use axum::{
-    routing::get,
-    Json,
-    Router,
+    extract::State, routing::get, Json, Router
 };
-use models::block::{
-    get_genesis_block,
-    is_valid_chain,
-    IBlock,
-    Block
+use tower_http::trace::{DefaultMakeSpan, TraceLayer};
+use routes::{blocks::block_router, peers::peers_router};
+use crate::models::block::{
+    get_genesis_block, is_valid_chain, Block, IBlock
 };
 mod models;
+mod routes;
 
-static mut BLOCKCHAIN: Vec::<Block> = Vec::<Block>::new();
+// Define your global state struct
+#[derive(Clone, Debug)]
+pub struct AppState {
+    blockchain: Arc<Mutex<Vec<Block>>>,
+}
 
 #[tokio::main]
 async fn main() {
+    let state = AppState {
+        blockchain: Arc::new(Mutex::new(Vec::<Block>::new().to_owned())),
+    };
 
-    initialize_blockchain();
+    let shared_state = Arc::new(Mutex::new(state));
+
+    initialize_blockchain(&shared_state);
     let app = Router::new()
-        .route("/", get(handler));
+        .route("/", get(handler))
+        .nest("/blocks", block_router(shared_state.clone()))
+        .nest("/peers", peers_router(shared_state.clone()))
+        // logging so we can see what's going on
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+        )
+        .with_state(shared_state);
 
     let addr = "0.0.0.0:3001";
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     println!("listening on http://{}", addr);
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
 }
 
-fn initialize_blockchain() {
-    unsafe { BLOCKCHAIN.push(get_genesis_block()) };
+fn initialize_blockchain(state: &Arc<Mutex<AppState>>) {
+    let state = state.lock().unwrap();
+    let mut blockchain = state.blockchain.lock().expect("blockchain was poisoned");
+
+    blockchain.push(get_genesis_block());
 }
 
-async fn handler() -> Json<Vec::<Block>> {
-    unsafe {
-        let last_block =  BLOCKCHAIN [BLOCKCHAIN.len() - 1].clone();
+async fn handler(State(state): State<Arc<Mutex<AppState>>>) -> Json<Vec::<Block>> {
+    let state = state.lock().unwrap();
+    let mut blockchain = state.blockchain.lock().expect("blockchain was poisoned");
+    let last_block =  blockchain[blockchain.len() - 1].clone();
 
-        let new_block = last_block.next_block(&String::from("new block"));
+    let new_block = last_block.next_block(&String::from("new block"));
 
-        BLOCKCHAIN.push(new_block);
+    blockchain.push(new_block);
 
-        if is_valid_chain(&BLOCKCHAIN.clone()) {
-            return Json(BLOCKCHAIN.clone());
-        }
-
-        BLOCKCHAIN.pop();
-
-        return Json(BLOCKCHAIN.clone())
+    if is_valid_chain(&blockchain.clone()) {
+        let mut response = blockchain.clone();
+        response.reverse();
+        return Json(response);
     }
+
+    let mut response = blockchain.clone();
+    response.reverse();
+
+    return Json(response)
 }
 
+pub fn replace_chain(State(state): State<AppState>, new_blocks: &Vec<Block>) {
+    let mut blockchain = state.blockchain.lock().expect("blockchain was poisoned");
 
-
-pub unsafe fn replace_chain(new_blocks: &Vec<Block>) {
-    if is_valid_chain(new_blocks) && new_blocks.len() > BLOCKCHAIN.len() {
-        BLOCKCHAIN = new_blocks.clone();
+    if is_valid_chain(new_blocks) && new_blocks.len() > blockchain.len() {
+        *blockchain = new_blocks.clone();
         // broadcast_latest_chain()
     }
 }
---
+++ src/routes/peers.rs
@@ -0,0 +1,171 @@
+use std::sync::{Arc, Mutex};
+use std::net::SocketAddr;
+use axum::{
+    body::Bytes,
+    extract::{
+        ws::{close_code, CloseFrame, Message, Utf8Bytes, WebSocket, WebSocketUpgrade},
+        State,
+        connect_info::ConnectInfo
+    }, response::IntoResponse, routing::any, Json, Router
+};
+use axum_extra::TypedHeader;
+use std::ops::ControlFlow;
+use futures::{sink::SinkExt, stream::StreamExt};
+
+use crate::AppState;
+
+pub fn peers_router(state: Arc<Mutex<AppState>>) -> Router<Arc<Mutex<AppState>>> {
+    Router::new()
+        .route("/", any(handle_root))
+        .route("/ws", any(handle_ws_upgrade))
+        .with_state(state)
+}
+
+async fn handle_root() -> impl IntoResponse {
+    println!("Peers root");
+
+    Json(String::from("Ok"))
+}
+
+async fn handle_ws_upgrade(
+    State(_state): State<Arc<Mutex<AppState>>>,
+    ws: WebSocketUpgrade,
+    user_agent: Option<TypedHeader<headers::UserAgent>>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    println!("Peers handshake");
+    let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
+        user_agent.to_string()
+    } else {
+        String::from("Unknown browser")
+    };
+
+    println!("`{user_agent}` at {addr} connected.");
+
+    // ()
+
+    return ws.on_upgrade(move |socket| handle_socket(socket, addr))
+}
+
+async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
+    if socket
+        .send(Message::Ping(Bytes::from_static(&[1, 2, 3])))
+        .await
+        .is_ok()
+    {
+        println!("Pinged {who}");
+    } else {
+        println!("Could not send ping to {who}");
+        return;
+    }
+
+    if let Some(msg) = socket.recv().await {
+        if let Ok(msg) = msg {
+            if process_message(msg, &who).is_break() {
+                return;
+            }
+        } else {
+            println!("client {who} abruptly disconnected");
+            return;
+        }
+    }
+
+    for i in 1..5 {
+        if socket
+            .send(Message::Text(format!("Hi {i} times!").into()))
+            .await
+            .is_err()
+        {
+            println!("client {who} abruptly disconnected");
+            return;
+        }
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    }
+
+    let (mut sender, mut receiver) = socket.split();
+    let mut send_task = tokio::spawn(async move {
+        let n_msg = 20;
+        for i in 0..n_msg {
+            if sender
+                .send(Message::Text(format!("Server message {i} ...").into()))
+                .await
+                .is_err()
+            {
+                return i;
+            }
+
+            tokio::time::sleep(std::time::Duration::from_millis(300)).await;
+        }
+
+        println!("Sending close to {who}...");
+        if let Err(e) = sender
+            .send(Message::Close(Some(CloseFrame {
+                code: close_code::NORMAL,
+                reason: Utf8Bytes::from_static("Goodbye"),
+            })))
+            .await
+        {
+            println!("Could not send Close due to {e}, probably it is ok?");
+        }
+        n_msg
+    });
+
+    let mut recv_task = tokio::spawn(async move {
+        let mut cnt = 0;
+        while let Some(Ok(msg)) = receiver.next().await {
+            cnt += 1;
+            if process_message(msg, &who).is_break() {
+                break;
+            }
+        }
+        cnt
+    });
+
+    tokio::select! {
+        rv_a = (&mut send_task) => {
+            match rv_a {
+                Ok(a) => println!("{a} messages sent to {who}"),
+                Err(a) => println!("Error sending messages {a:?}")
+            }
+            recv_task.abort();
+        },
+        rv_b = (&mut recv_task) => {
+            match rv_b {
+                Ok(b) => println!("Received {b} messages"),
+                Err(b) => println!("Error receiving messages {b:?}")
+            }
+            send_task.abort();
+        }
+    }
+
+    println!("Websocket context {who} destroyed");
+}
+
+fn process_message(msg: Message, who: &SocketAddr) -> ControlFlow<(), ()> {
+    match msg {
+        Message::Text(t) => {
+            println!(">>> {who} sent str: {t:?}");
+        }
+        Message::Binary(d) => {
+            println!(">>> {} sent {} bytes: {:?}", who, d.len(), d);
+        }
+        Message::Close(c) => {
+            if let Some(cf) = c {
+                println!(
+                    ">>> {} sent close with code {} and reason `{}`",
+                    who, cf.code, cf.reason
+                );
+            } else {
+                println!(">>> {who} somehow sent close message without CloseFrame");
+            }
+            return ControlFlow::Break(());
+        }
+        Message::Pong(v) => {
+            println!(">>> {who} sent pong with {v:?}");
+        }
+        Message::Ping(v) => {
+            println!(">>> {who} sent ping with {v:?}");
+        }
+    }
+    ControlFlow::Continue(())
+}
