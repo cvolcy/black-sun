6d4abec6fb7d610b21060499a4997ab56d819286
Clervens Volcy <clervens.volcy@gmail.com>
2025-03-15 21:22:50 -0400

    Added mine route

--- src/routes/blocks.rs
+++ src/routes/blocks.rs
@@ -1,10 +1,18 @@
 use std::sync::{Arc, Mutex};
-use axum::{extract::State, routing::get, Json, Router};
-use crate::{models::block::Block, AppState};
+use axum::debug_handler;
+use axum::{extract::State, response::IntoResponse, routing::{get, post}, Form, Json, Router};
+use serde::Deserialize;
+use crate::{models::block::{Block, IBlock}, AppState};
+
+#[derive(Deserialize)]
+struct MineBlockDataRequest {
+    block_data: String
+}
 
 pub fn block_router(state: Arc<Mutex<AppState>>) -> Router<Arc<Mutex<AppState>>> {
     return Router::new()
         .route("/", get(get_blocks))
+        .route("/mine", post(mine_block))
         .with_state(state);
     // router.route("/mineBlock",);
 }
@@ -17,4 +25,19 @@ async fn get_blocks(State(state): State<Arc<Mutex<AppState>>>) -> Json<Vec::<Blo
     response.reverse();
 
     return Json(response)
+}
+
+async fn mine_block(
+    State(state): State<Arc<Mutex<AppState>>>,
+    Form(mbdr): Form<MineBlockDataRequest>
+) -> impl IntoResponse {
+    let state = state.lock().unwrap();
+    let mut blockchain = state.blockchain.lock().unwrap();
+
+    let last_block = blockchain[blockchain.len() - 1].clone();
+    let new_block = last_block.next_block(&mbdr.block_data);
+    let response = new_block.clone();
+    
+    blockchain.push(new_block);
+    Json(response)
 }
